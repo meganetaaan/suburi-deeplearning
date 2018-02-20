71ff6472e1f629e90722fe8f3a2f677e489a3b5c
Shinya Ishikawa <ishikawa.s.1027@gmail.com>
2018-02-20 12:28:35 +0900

    Add deeplearnjs

--- vue-deeplearn/src/components/DeepLearning.vue
+++ vue-deeplearn/src/components/DeepLearning.vue
@@ -1,14 +1,24 @@
 <template>
-  <div class="deepLearning"></div>
+  <div class="deepLearning">hoge</div>
 </template>
 
 <script>
+import * as dl from 'deeplearn'
+console.log(dl)
+
 export default {
   name: 'DeepLearning',
   data () {
     return {
       msg: 'Welcome to Your Vue.js App'
     }
+  },
+  mounted () {
+    const a = dl.tensor1d([1, 2, 3])
+    const b = dl.scalar(2)
+
+    const result = a.add(b)
+    result.data().then(data => console.log(data))
   }
 }
 </script>
